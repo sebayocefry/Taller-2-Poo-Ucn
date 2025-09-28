bc6197c904c87cc4a7cb9953ea66807bd26afc7f
Sebastian <sebastianb817@gmail.com>
2025-09-28 13:32:31 -0300

    Cuarto comit- mostrar usuario vista

--- src/secureNet.java
+++ src/secureNet.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 // Clase PC
@@ -136,7 +139,7 @@ class Vulnerabilidad{
 }
 
 // Clase Usuario
-class Usuario {
+class Usuario{
     private String username;
     private String passwor;
     private String rolUser;
@@ -174,5 +177,100 @@ class Sistema{
     private ArrayList<PC> listaPc;
     private ArrayList<Usuario> listaUsuarios;
     private ArrayList<Vulnerabilidad> listaVulnerabilidades;
+
+    //cargas comunes de archivos.txt
+    public void cargarPCs(String archivo)throws FileNotFoundException{ 
+        File arch = new File(archivo);
+        Scanner lector = new Scanner(arch);
+
+        while (lector.hasNextLine()){
+            String linea = lector.nextLine().trim();
+            String[] partes = linea.split("\\|");//java confunde el separador con el or 
+            listaPc.add(new PC(partes[0], partes[1], partes[2]));
+        }
+        lector.close();
+    }
+    
+
+
+    public void cargarPuertos(String archivo)throws FileNotFoundException{ 
+        File file = new File(archivo);
+        Scanner lector = new Scanner(file);
+        while(lector.hasNextLine()){
+            String linea = lector.nextLine().trim();
+            String[] partes = linea.split("\\|");
+            String iDpc = partes[0];// punto para no perder la relacion entre los pc
+            int nPuerto = Integer.parseInt(partes[1]);
+            boolean estadoPuerto = partes[2].equalsIgnoreCase("Abierto");// con esto java compara buscando abierto-AbIerTo-1 para true sino false
+
+            // me muevo como un pc en la lista de pc iniciada comparo por id hasta encontrar una coincidencia, cuando la encuentro llamo a la lista puertos (sigue)
+            // agrego partes 1 y 2 
+            for (PC elem : listaPc) {
+                if(elem.getiD().equals(iDpc)){
+                    elem.getPuertos().add(new Puerto(nPuerto, estadoPuerto));
+                    break;
+                }
+                
+            }
+
+        }
+        lector.close();
+
+    }
+    public void cargarUsuarios(String archivo)throws FileNotFoundException{
+        File file = new File(archivo);
+        Scanner lector = new Scanner(file);
+
+        while(lector.hasNextLine()){
+            String linea = lector.nextLine().trim();
+            String [] partes = linea.split(";");
+            listaUsuarios.add(new Usuario(partes[0], partes[1], partes[2]));
+        }
+
+        lector.close();
+    }
+    public void cargarVulnerabilidades(String archivo)throws FileNotFoundException{
+        File arch = new File(archivo);
+        Scanner lector = new Scanner(arch);
+        
+        while(lector.hasNextLine()){
+            String linea = lector.nextLine().trim();
+            String[] partes = linea.split("\\|");
+            listaVulnerabilidades.add(new Vulnerabilidad(Integer.parseInt(partes[0]), partes[1], partes[2]));// el parseint y no el valueOf, para que me lo convierta un entero primitivo
+        }
+
+        lector.close();
+    }
+
+    //opciones para usario 
+
+    public void mostrarPcsUser(){
+        //imprimir ID, IP, SO
+        // sin metodo toatring porque quiero imprimir ciertos datos y no todos como en el usuario 
+        if (listaPc.isEmpty()){
+            System.out.println("La lista se encuentra vacia");
+        }else{
+            for (PC elem : listaPc) {
+            System.out.println("PC ID: "+elem.getiD());
+            System.out.println("PC IP: "+elem.getiP());
+            System.out.println("PC SO: "+elem.getSistemaOperativo());
+            System.err.println("PUERTOS:");
+            for (Puerto puertos : elem.getPuertos()) {
+                String estado;
+                if(puertos.isEstadoPuerto()){
+                    estado = "ABIERTO";
+                }else{
+                    estado = "Cerrado";
+                }
+                System.out.println("ESTADO DEL PUERTO "+puertos.getNumeroPuero()+ ": " + estado);
+            }
+            System.out.println();
+            System.out.println("---------------------------");
+
+         }
+
+        }
+    
+    }
 }
 
