49dbf5898f8a732fd5b22a6066e3acff455db72a
Sebastian <sebastianb817@gmail.com>
2025-09-30 12:22:13 -0300

    txt actualizacion 1

--- src/secureNet.java
+++ src/secureNet.java
@@ -1,10 +1,10 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.FileWriter;
 
 
 // Clase PC
